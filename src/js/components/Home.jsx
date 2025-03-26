d93b2d24e5cf9135ae6ac4b544b3265bdfaed333
victorpallas1 <victorpallas@seguridadea1.com>
2025-03-26 13:44:24 +0000

    cambios finales

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 // Crear tu primer componente
 const Home = () => {
     const [newTarea, setNewTarea] = useState("");
-    const [tareas, setTareas] = useState([""]);
+    const [tareas, setTareas] = useState([]);
     const [hoverIndex, setHoverIndex] = useState(null); // Para mostrar el botón al pasar el mouse
 
     function agregarTarea() {
@@ -17,12 +17,19 @@ const Home = () => {
         setTareas(nuevasTareas);
     }
 
+    function eliminarTodas() {
+        setTareas([]); // Vacía la lista de tareas
+    }
+
     return (
         <div className="m-auto text-center w-50">
+            <h1>Mis tareas</h1>
+            
             <input
                 type="text"
                 value={newTarea}
                 onChange={(e) => setNewTarea(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && agregarTarea()} // Agregar tarea al presionar Enter
                 placeholder="Escribe tu tarea"
                 className="form-control mb-2"
             />
@@ -44,12 +51,18 @@ const Home = () => {
                                 className="btn btn-danger btn-sm"
                                 onClick={() => eliminarTarea(index)}
                             >
-                                x
+                                DELETE
                             </button>
                         )}
                     </li>
                 ))}
             </ul>
+            <p></p>
+            <span className="mb-3">Total: {tareas.length}</span>
+               
+            <button onClick={eliminarTodas} className="btn btn-danger">
+               Eliminar todas
+            </button>
         </div>
     );
 };
